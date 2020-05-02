0988819d1577bd3a50798ee2b5afb1fedfa756ac
Destiny Rose <destinyrose2358@gmail.com>
2020-05-02 18:18:11 -0500

    Adds functions to sales tax util

--- src/utils/sales_tax_calculator.js
+++ src/utils/sales_tax_calculator.js
@@ -1,3 +1,37 @@
-function findTaxesFor({ categories, basePrice, quantity }) {
-    
+const CategoriesTaxRates = {
+    food: 0,
+    medical: 0,
+    book: 0,
+    basic: 0.1,
+    imported: 0.05,
 }
+
+const findSalesTaxFor = ({ categories, basePrice, quantity }) => {
+    let salesTaxRate = categories.reduce((currentTotal, categorie) => {
+        return currentTotal + CategoriesTaxRates[categorie];
+    });
+    return salesTaxRate * basePrice * quantity;
+};
+
+const generateItemName = () => {
+    
+};
+
+const calculateReceipt = (items) => {
+    let finalItemPrices = [];
+    let totalSalesTax;
+    items.reduce(({finalItemPrices, totalSalesTax, total}, item) => {
+        const salesTax = findSalesTaxFor(item);
+        const subtotal = item.basePrice + salesTax;
+
+        finalItemPrices[]
+        return {
+            finalItemPrices: [...finalItemPrices, subtotal],
+            totalSalesTax: 
+        }
+    }, {
+        finalItemPrices: {},
+        totalSalesTax: 0,
+        total: 0
+    })
+};
