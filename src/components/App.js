6436f6a86ffb7a7a157575bb0c4701764e06f686
Destiny Rose <destinyrose2358@gmail.com>
2020-05-04 13:20:26 -0500

    Fixes import statements

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, Switch } from "react-router-dom"
 import logo from '../logo.svg';
-import '../stylings/App.css';
+import '../stylings/App.scss';
 import SalesInput from './sales-input/sales-input';
 import ShoppingCart from '../utils/sales_tax/shopping_cart';
 
